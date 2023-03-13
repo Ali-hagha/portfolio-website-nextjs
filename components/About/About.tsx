d80ee32809a214c6a69ea6470e56e809685c3f81
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-03-13 20:45:14 +0330

    style about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,8 +1,33 @@
+import Image from 'next/image';
+import { TbDiamondsFilled } from 'react-icons/tb';
+
 const About = () => {
   return (
-    <section id="about">
+    <section
+      id="about"
+      className="mb-48 flex flex-col lg:flex-row items-center h-full"
+    >
+      <div className="flex flex-col relative h-56 sm:h-80 lg:h-auto  w-screen  items-center justify-center mb-4 md:mb-8 lg:mb-0 lg:mr-8 lg:flex-1 lg:self-stretch">
+        <div className="flex flex-col items-center justify-center lg:self-stretch">
+          <h2 className="text-3xl font-bold">About Me</h2>
+          <Image
+            src={'/about underline.svg'}
+            alt={'underline'}
+            width="160"
+            height="200"
+          />
+        </div>
+        <Image
+          src={'/about flash art.svg'}
+          alt={'flash'}
+          width="1024"
+          height="1024"
+          className="absolute right-0 left-0 scale-75 lg:scale-90 -z-40"
+        />
+        <div className='bg-[url("/organic-pattern.svg")] absolute top-0 bottom-0 left-0 right-0 -z-50 bg-cover'></div>
+      </div>
       <div>
-        <div className="text-base md:text-lg tracking-wide max-w-lg leading-relaxed">
+        <div className="text-base md:text-lg tracking-wide max-w-lg md:max-w-xl lg:max-w-lg xl:max-w-2xl leading-relaxed mb-4">
           <p>
             Hi there! My name is Ali, and I am an ambitious new developer on the
             lookout for exciting job opportunities in front-end development. I
@@ -20,16 +45,33 @@ const About = () => {
             These are some of the latest technologies I have been working with:
           </p>
         </div>
-        <ul>
-          <li>Javascript (ES6+)</li>
-          <li>React</li>
-          <li>Tailwind</li>
-          <li>Typescript</li>
-          <li>Next.js</li>
-          <li>React Native</li>
+        <ul className="columns-2 text-sm md:text-base">
+          <li>
+            <TbDiamondsFilled className="inline-block mr-1 text-primary" />
+            Javascript (ES6+)
+          </li>
+          <li>
+            <TbDiamondsFilled className="inline-block mr-1 text-primary" />
+            React
+          </li>
+          <li>
+            <TbDiamondsFilled className="inline-block mr-1 text-primary" />
+            Tailwind
+          </li>
+          <li>
+            <TbDiamondsFilled className="inline-block mr-1 text-primary" />
+            Typescript
+          </li>
+          <li>
+            <TbDiamondsFilled className="inline-block mr-1 text-primary" />
+            Next.js
+          </li>
+          <li>
+            <TbDiamondsFilled className="inline-block mr-1 text-primary" />
+            React Native
+          </li>
         </ul>
       </div>
-      <div></div>
     </section>
   );
 };
