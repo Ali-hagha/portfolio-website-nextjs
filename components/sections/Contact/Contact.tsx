1383884d0dae3cd2b93de95f78cbb11873c3dc35
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-03-25 19:07:11 +0330

    add hrefs to socia and email links

--- components/sections/Contact/Contact.tsx
+++ components/sections/Contact/Contact.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 const Contact = () => {
   return (
     <section id="contact" className="mb-36 scroll-mt-24 md:scroll-mt-32 ">
@@ -12,10 +14,13 @@ const Contact = () => {
           Are you are interested in hiring me or have any questions about my
           work? Go ahead.
         </p>
-        <div className='flex flex-col items-center justify-center before:py-16  w-96  relative before:absolute before:-z-50 before:-top-[calc(64px-22px)] before:left-0   before:w-full before:h-full before:bg-[url("/arrows.svg")] before:bg-no-repeat before:bg-contain before:bg-center before:drop-shadow-[5px_5px_0px_rgba(28,53,53,1)] before:block md:before:hidden'>
-          <button className="rounded-lg py-2 md:py-3 px-4 md:px-10 bg-primary text-white font-medium text-lg md:text-xl hover:bg-primary-dark active:bg-primary-dark transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">
+        <div className='flex flex-col items-center justify-center mt-6 md:mt-0 before:py-20   w-full  relative before:absolute before:-z-50 before:-top-[calc(80px-22px)] before:left-0   before:w-full before:h-full before:bg-[url("/arrows.svg")] before:bg-no-repeat before:bg-contain before:bg-center before:drop-shadow-[5px_5px_0px_rgba(28,53,53,1)] before:block md:before:hidden'>
+          <Link
+            href={'mailto:ali.haghayegh@gmail.com'}
+            className="rounded-lg py-2 md:py-3 px-4 md:px-10 bg-primary text-white font-medium text-lg md:text-xl hover:bg-primary-dark active:bg-primary-dark transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary"
+          >
             Say Hello 👋
-          </button>
+          </Link>
         </div>
       </div>
     </section>
--- components/sections/Footer/Footer.tsx
+++ components/sections/Footer/Footer.tsx
@@ -13,20 +13,20 @@ const Footer = () => {
       <div>
         <div className="flex flex-row items-center justify-center space-x-10 md:space-x-16 text-2xl text-white ">
           <Link
-            href={'#'}
-            className="p-2 inline-block hover:text-[#fff30a]  transition-colors"
+            href={'https://www.linkedin.com/in/ali-haghayegh-172a331a5/#'}
+            className="p-2 inline-block hover:text-[#fff30a]  transition-all hover:-translate-y-1"
           >
             <FiLinkedin />
           </Link>
           <Link
-            href={'#'}
-            className="p-2 inline-block hover:text-[#fff30a]  transition-colors"
+            href={'https://github.com/Ali-hagha'}
+            className="p-2 inline-block hover:text-[#fff30a]  transition-all hover:-translate-y-1"
           >
             <FiGithub />
           </Link>
           <Link
-            href={'#'}
-            className="p-2 inline-block hover:text-[#fff30a]  transition-colors"
+            href={'https://t.me/Ali_hagha'}
+            className="p-2 inline-block hover:text-[#fff30a]  transition-all hover:-translate-y-1 "
           >
             <TbBrandTelegram />
           </Link>
