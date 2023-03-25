3c25b714d41a80dbba83c01fc698d07e65e99ad3
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-03-25 19:37:05 +0330

    add social links to sidedrawer

--- components/ui/SideDrawer/SideDrawer.tsx
+++ components/ui/SideDrawer/SideDrawer.tsx
@@ -4,6 +4,9 @@ import { Transition } from '@headlessui/react';
 import Backdrop from './Backdrop';
 import DrawerNavItemGroup from './DrawerNavItemGroup';
 import DrawerNavItem from './DrawerNavItem';
+import Link from 'next/link';
+import { FiGithub, FiLinkedin } from 'react-icons/fi';
+import { TbBrandTelegram } from 'react-icons/tb';
 
 type props = {
   isSideDrawerActive: boolean;
@@ -26,7 +29,7 @@ const SideDrawer = ({ isSideDrawerActive, setIsSideDrawerActive }: props) => {
   };
 
   return (
-    <div className="md:hidden">
+    <div className="md:hidden ">
       <Backdrop
         isSideDrawerActive={isSideDrawerActive}
         setIsSideDrawerActive={setIsSideDrawerActive}
@@ -59,6 +62,26 @@ const SideDrawer = ({ isSideDrawerActive, setIsSideDrawerActive }: props) => {
               CONTACT
             </DrawerNavItem>
           </DrawerNavItemGroup>
+          <div className="flex flex-row absolute bottom-10 items-center justify-center space-x-10 md:space-x-16 text-2xl text-white ">
+            <Link
+              href={'https://www.linkedin.com/in/ali-haghayegh-172a331a5/#'}
+              className="p-2 inline-block hover:text-[#fff30a]  transition-all hover:-translate-y-1"
+            >
+              <FiLinkedin />
+            </Link>
+            <Link
+              href={'https://github.com/Ali-hagha'}
+              className="p-2 inline-block hover:text-[#fff30a]  transition-all hover:-translate-y-1"
+            >
+              <FiGithub />
+            </Link>
+            <Link
+              href={'https://t.me/Ali_hagha'}
+              className="p-2 inline-block hover:text-[#fff30a]  transition-all hover:-translate-y-1 "
+            >
+              <TbBrandTelegram />
+            </Link>
+          </div>
         </aside>
       </Transition>
     </div>
