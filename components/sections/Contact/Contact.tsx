71aa5d5204571836d28d15bd9dbe3a97ed29ff7c
Ali Haghayegh <ali.haghayegh@gmail.com>
2023-03-25 19:19:51 +0330

    remove scroll margins

--- components/sections/Contact/Contact.tsx
+++ components/sections/Contact/Contact.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const Contact = () => {
   return (
-    <section id="contact" className="mb-36 scroll-mt-24 md:scroll-mt-32 ">
+    <section id="contact" className="mb-36  ">
       <div className='flex flex-col items-center justify-center py-24 w-full relative before:absolute before:-z-50 before:top-0 before:left-0   before:w-full before:h-full before:bg-[url("/arrows.svg")] before:bg-no-repeat before:bg-contain before:bg-center before:drop-shadow-[5px_5px_0px_rgba(28,53,53,1)] before:hidden md:before:block'>
         <h2 className=" font-light text-base md:text-lg tracking-wide mb-4 text-center">
           Like what you see?
