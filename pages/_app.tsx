beafd1d915fd9be86fdb293e3ac713062768fb8f
Ali home pc <ali.haghayegh@gmail.com>
2024-01-16 16:18:26 +0330

    add Ackee analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css';
-import type { AppProps } from 'next/app';
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />;
+      <Script
+        strategy="afterInteractive"
+        src="https://analytics-test.liara.run/tracker.js"
+        data-ackee-server="https://analytics-test.liara.run"
+        data-ackee-domain-id="2c96a2f8-2cce-43bc-a315-a5a1ae4acaea"
+      />
+    </>
+  );
 }
