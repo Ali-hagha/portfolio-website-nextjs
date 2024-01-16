14723e0c318ef117b4761f3ce7202aa70b8e740e
Ali home pc <ali.haghayegh@gmail.com>
2024-01-16 23:07:55 +0330

    remove analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />;
-      <Script
-        strategy="afterInteractive"
-        src="https://analytics-test.liara.run/tracker.js"
-        data-ackee-server="https://analytics-test.liara.run"
-        data-ackee-domain-id="2c96a2f8-2cce-43bc-a315-a5a1ae4acaea"
-      />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
