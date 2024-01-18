43fb016a938de3682f1b4abe46336012fc617535
Ali home pc <ali.haghayegh@gmail.com>
2024-01-18 16:01:50 +0330

    add matomo analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,27 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script id="matotmo">
+        {`
+      var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics-test.liara.run/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '1']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+      
+      `}
+      </Script>
+    </>
+  );
 }
