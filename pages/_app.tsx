72e8c0c9d5a53d13ad498e87feff237d8ba6d1fd
Ali home pc <ali.haghayegh@gmail.com>
2024-01-22 23:49:59 +0330

    add plausible analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Script id="matotmo">
-        {`
-      var _paq = window._paq = window._paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//analytics-test.liara.run/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '1']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-  })();
-      
-      `}
-      </Script>
+      <Script
+        defer
+        strategy="afterInteractive"
+        data-domain="alihaghayegh.ir"
+        src="https://analytics.alihaghayegh.ir/js/script.js"
+      />
     </>
   );
 }
